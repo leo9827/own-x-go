f56b8882a72af0ead5d939dd9abf20db66d1b8cc
leo9827 <741829374@qq.com>
2024-01-06 21:55:43 +0800

    update

---
+++ concurrency/concurrency_pattern_test.go
@@ -0,0 +1,672 @@
+package concurrency
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestConfinement(t *testing.T) {
+	data := make([]int, 4)
+
+	loopData := func(handleData chan<- int) {
+		defer close(handleData)
+		for i := range data {
+			handleData <- i
+		}
+	}
+
+	handleData := make(chan int)
+
+	go loopData(handleData)
+
+	for num := range handleData {
+		fmt.Println(num)
+	}
+}
+
+func TestConfinement2(t *testing.T) {
+	chanOwner := func() <-chan int {
+		results := make(chan int, 5)
+		go func() {
+			defer close(results)
+			for i := 0; i <= 5; i++ {
+				results <- i
+			}
+		}()
+		return results
+	}
+
+	consumer := func(results <-chan int) {
+		for result := range results {
+			fmt.Printf("Received: %d\n", result)
+		}
+		fmt.Println("Done receiving!")
+	}
+	results := chanOwner()
+	consumer(results)
+}
+
+func TestConfinement3(t *testing.T) {
+	printData := func(wg *sync.WaitGroup, data []byte) {
+		defer wg.Done()
+		var buf bytes.Buffer
+
+		for _, b := range data {
+			fmt.Fprintf(&buf, "%c", b)
+		}
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	var data = []byte("golang")
+
+	go printData(&wg, data[:3])
+	go printData(&wg, data[3:])
+
+	wg.Wait()
+}
+
+func TestForSelect(t *testing.T) {
+	done := make(chan interface{})
+	go func() {
+		time.Sleep(5 * time.Second)
+		done <- struct{}{}
+	}()
+loop:
+	for { // Either loop infinitely or range over something
+		select {
+		// Do some work with channels
+		case <-done:
+		case <-time.After(2 * time.Second):
+			fmt.Println("timer after")
+			break loop
+		}
+	}
+}
+
+func TestLeaks(t *testing.T) {
+	doWork := func(strings <-chan string) <-chan string {
+		completed := make(chan string)
+		go func() {
+			defer fmt.Println("doWork exited.")
+			defer close(completed)
+			for s := range strings {
+				fmt.Println(s)
+			}
+		}()
+		return completed
+	}
+
+	doWork(nil)
+	fmt.Println("Done.")
+}
+
+func TestLeaks2(t *testing.T) {
+	doWork := func(
+		done <-chan interface{},
+		strings <-chan string,
+	) <-chan interface{} {
+		terminated := make(chan interface{})
+		go func() {
+			defer fmt.Println("doWork exited.")
+			defer close(terminated)
+			for {
+				select {
+				case s := <-strings:
+					fmt.Println(s)
+				case <-done:
+					return
+				}
+			}
+		}()
+		return terminated
+	}
+
+	done := make(chan interface{})
+	terminated := doWork(done, nil)
+
+	go func() {
+		// Cancel the operation after 1 second
+		time.Sleep(1 * time.Second)
+		fmt.Println("Cancel doWork goroutine...")
+		close(done)
+	}()
+
+	<-terminated
+	fmt.Println("Done.")
+
+}
+
+func TestOrChannel(t *testing.T) {
+	var or func(channels ...<-chan interface{}) <-chan interface{}
+	or = func(channels ...<-chan interface{}) <-chan interface{} {
+		switch len(channels) {
+		case 0:
+			return nil
+		case 1:
+			return channels[0]
+		}
+
+		orDone := make(chan interface{})
+		go func() {
+			defer close(orDone)
+
+			switch len(channels) {
+			case 2:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				}
+			default:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				case <-channels[2]:
+				case <-or(append(channels[3:], orDone)...):
+				}
+			}
+		}()
+
+		return orDone
+	}
+
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(3*time.Second),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+	fmt.Printf("done after %v\n", time.Since(start)) // done after 1.xs
+}
+
+func TestError(t *testing.T) {
+	checkStatus := func(
+		done <-chan interface{},
+		urls ...string,
+	) <-chan *http.Response {
+		responses := make(chan *http.Response)
+		go func() {
+			defer close(responses)
+			for _, url := range urls {
+				resp, err := http.Get(url)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				select {
+				case <-done:
+					return
+				case responses <- resp:
+				}
+			}
+		}()
+		return responses
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	urls := []string{"https://www.google.com", "https://badhost"}
+	for response := range checkStatus(done, urls...) {
+		fmt.Printf("Response: %v\n", response.Status)
+	}
+}
+
+func TestErrHandling(t *testing.T) {
+	type Result struct {
+		Error    error
+		Response *http.Response
+	}
+
+	checkStatus := func(
+		done <-chan interface{},
+		urls ...string,
+	) <-chan Result {
+		results := make(chan Result)
+		go func() {
+			defer close(results)
+			for _, url := range urls {
+				resp, err := http.Get(url)
+				result := Result{Error: err, Response: resp}
+				select {
+				case <-done:
+					return
+				case results <- result:
+				}
+			}
+		}()
+		return results
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	urls := []string{"https://www.google.com", "https://badhost"}
+	for result := range checkStatus(done, urls...) {
+		if result.Error != nil {
+			fmt.Printf("error: %v", result.Error)
+			continue
+		}
+		fmt.Printf("Response: %v\n", result.Response)
+	}
+}
+
+func TestPipelines(t *testing.T) {
+	multiply := func(values []int, multiplier int) []int {
+		multipliedValues := make([]int, len(values))
+		for i, v := range values {
+			multipliedValues[i] = v * multiplier
+		}
+		return multipliedValues
+	}
+
+	add := func(values []int, additive int) []int {
+		addedValues := make([]int, len(values))
+		for i, v := range values {
+			addedValues[i] = v + additive
+		}
+		return addedValues
+	}
+
+	ints := []int{1, 2, 3, 4}
+	for _, v := range add(multiply(ints, 2), 1) {
+		fmt.Println(v)
+	}
+}
+
+func TestBestPipelines(t *testing.T) {
+	generator := func(done <-chan interface{}, integers ...int) <-chan int {
+		intStream := make(chan int)
+		go func() {
+			defer close(intStream)
+			for _, i := range integers {
+				select {
+				case <-done:
+					return
+				case intStream <- i:
+				}
+			}
+		}()
+		return intStream
+	}
+
+	multiply := func(
+		done <-chan interface{},
+		intStream <-chan int,
+		multiplier int,
+	) <-chan int {
+		multipliedStream := make(chan int)
+		go func() {
+			defer close(multipliedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case multipliedStream <- i * multiplier:
+				}
+			}
+		}()
+		return multipliedStream
+	}
+
+	add := func(
+		done <-chan interface{},
+		intStream <-chan int,
+		additive int,
+	) <-chan int {
+		addedStream := make(chan int)
+		go func() {
+			defer close(addedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case addedStream <- i + additive:
+				}
+			}
+		}()
+		return addedStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	intStream := generator(done, 1, 2, 3, 4)
+	pipeline := multiply(done, add(done, multiply(done, intStream, 2), 1), 2)
+
+	for v := range pipeline {
+		fmt.Println(v)
+	}
+}
+
+func BenchmarkGeneric(b *testing.B) {
+	done := make(chan interface{})
+	defer close(done)
+	toString := func(
+		done <-chan interface{}, valueStream <-chan interface{},
+	) <-chan string {
+		stringStream := make(chan string)
+		go func() {
+			defer close(stringStream)
+			for v := range valueStream {
+				select {
+				case <-done:
+					return
+				case stringStream <- v.(string):
+				}
+			}
+		}()
+		return stringStream
+	}
+	take := func(
+		done <-chan interface{}, valueStream <-chan interface{}, num int,
+	) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+	repeat := func(
+		done <-chan interface{}, values ...interface{},
+	) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				for _, v := range values {
+					select {
+					case <-done:
+						return
+					case valueStream <- v:
+					}
+				}
+			}
+		}()
+		return valueStream
+	}
+	b.ResetTimer()
+	for range toString(done, take(done, repeat(done, "a"), b.N)) {
+	}
+}
+
+func BenchmarkTyped(b *testing.B) {
+	repeat := func(done <-chan interface{}, values ...string) <-chan string {
+		valueStream := make(chan string)
+		go func() {
+			defer close(valueStream)
+			for {
+				for _, v := range values {
+					select {
+					case <-done:
+						return
+					case valueStream <- v:
+					}
+				}
+			}
+		}()
+		return valueStream
+	}
+	take := func(
+		done <-chan interface{}, valueStream <-chan string, num int,
+	) <-chan string {
+		takeStream := make(chan string)
+		go func() {
+			defer close(takeStream)
+			for i := num; i > 0 || i == -1; {
+				if i != -1 {
+					i--
+				}
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+	done := make(chan interface{})
+	defer close(done)
+	b.ResetTimer()
+	for range take(done, repeat(done, "a"), b.N) {
+	}
+}
+
+func TestFanOut(t *testing.T) {
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan string,
+		num int,
+	) <-chan string {
+		takeStream := make(chan string)
+		go func() {
+			close(takeStream)
+			for i := num; i > 0 || i == -1; {
+				if i != -1 {
+					i--
+				}
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	repeatFn := func(
+		done <-chan interface{},
+		fn func() interface{},
+	) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				select {
+				case <-done:
+					return
+				case valueStream <- fn():
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	toInt := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+	) <-chan int {
+		intStream := make(chan int)
+		go func() {
+			defer close(intStream)
+			for v := range valueStream {
+				select {
+				case <-done:
+					return
+				case intStream <- v.(int):
+				}
+			}
+		}()
+		return intStream
+	}
+	primeFinder := func(done <-chan interface{}, valueStream <-chan int) <-chan string {
+		primes := make(chan string)
+		go func() {
+			defer close(primes)
+			for v := range valueStream {
+				select {
+				case <-done:
+					return
+				case primes <- string(rune(v)):
+				}
+			}
+		}()
+		return primes
+	}
+	rand1 := func() interface{} { return rand.Intn(50000000) }
+	done := make(chan interface{})
+	defer close(done)
+
+	start := time.Now()
+
+	randIntStream := toInt(done, repeatFn(done, rand1))
+	fmt.Println("Primes:")
+	for prime := range take(done, primeFinder(done, randIntStream), 10) {
+		fmt.Printf("\t%s\n", prime)
+	}
+	fmt.Printf("Search took: %v", time.Since(start))
+}
+
+func TestFanIn(t *testing.T) {
+	fanIn := func(
+		done <-chan interface{},
+		channels ...<-chan interface{},
+	) <-chan interface{} {
+		var wg sync.WaitGroup
+		multiplexedStream := make(chan interface{})
+
+		multiplex := func(c <-chan interface{}) {
+			defer wg.Done()
+			for i := range c {
+				select {
+				case <-done:
+					return
+				case multiplexedStream <- i:
+				}
+			}
+		}
+
+		// Select from all the channels
+		wg.Add(len(channels))
+		for _, c := range channels {
+			go multiplex(c)
+		}
+
+		// Wait for all the reads to complete
+		go func() {
+			wg.Wait()
+			close(multiplexedStream)
+		}()
+
+		return multiplexedStream
+	}
+
+	toInt := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+	) <-chan int {
+		intStream := make(chan int)
+		go func() {
+			defer close(intStream)
+			for v := range valueStream {
+				select {
+				case <-done:
+					return
+				case intStream <- v.(int):
+				}
+			}
+		}()
+		return intStream
+	}
+
+	repeatFn := func(
+		done <-chan interface{},
+		fn func() interface{},
+	) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				select {
+				case <-done:
+					return
+				case valueStream <- fn():
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+		num int,
+	) <-chan int {
+		takeStream := make(chan int)
+		go func() {
+			close(takeStream)
+			for i := num; i > 0 || i == -1; {
+				if i != -1 {
+					i--
+				}
+				select {
+				case <-done:
+					return
+				case takeStream <- (<-valueStream).(int):
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	primeFinder := func(
+		done <-chan interface{},
+		valueStream <-chan int,
+	) <-chan interface{} {
+		primes := make(chan interface{})
+		go func() {
+			defer close(primes)
+			for v := range valueStream {
+				select {
+				case <-done:
+					return
+				case primes <- string(rune(v)):
+				}
+			}
+		}()
+		return primes
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+	start := time.Now()
+	rand1 := func() interface{} { return rand.Intn(50000000) }
+	randIntStream := toInt(done, repeatFn(done, rand1))
+	numFinders := runtime.NumCPU()
+	fmt.Printf("Spinning up %d prime finders.\n", numFinders)
+	finders := make([]<-chan interface{}, numFinders)
+	fmt.Println("Primes:")
+	for i := 0; i < numFinders; i++ {
+		finders[i] = primeFinder(done, randIntStream)
+	}
+	for prime := range take(done, fanIn(done, finders...), 10) {
+		fmt.Printf("\t%d\n", prime)
+	}
+	fmt.Printf("Search took: %v", time.Since(start))
+}
